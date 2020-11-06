c1bd949dc4f9abb91dc22873ba9a3225e97bd79c
tmwassenberg <tm.wassenberg@gmail.com>
2020-11-06 11:49:26 +0100

    Alter prototypes property from csvLine to word, implement that in algorithm

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const { renderAdjectives } = require("./lib/render");
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 5000;
 
 if (!process.argv[2]) {
-	console.log("Please submtit a text input as in: '<start_command> text_file'");
+	console.log("Please submtit a text input as in: '<start_command> path/to/text_file'");
 	return;
 }
-
-const express = require("express");
-const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
 app.listen( port, ( res ) => {
 	console.log("App listening in port " + port);
 });
-
 
 renderAdjectives(process.argv[2]);
